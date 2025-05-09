55a1e8ac589ca9b58f8c2342378c20bb2835c819
tkdlrs <tkd.lsanchez@gmail.com>
2025-05-09 17:08:22 -0600

    More minor tweaks to finish up 'Lesson 5: Help and Exit' better.

--- command_help.go
+++ command_help.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 
 func commandHelp() error {
-	outputString := "help: Displays a help message\nexit: Exit the Pokedex\n"
-	fmt.Printf("Welcome to the Pokedex! \nUsage: \n\n%v", outputString)
+	fmt.Print("\nWelcome to the Pokedex!\nUsage:\n")
 	for _, cmd := range getCommands() {
-		fmt.Printf("%v: %v", cmd.name, cmd.description)
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
 	}
 	fmt.Println()
 	return nil
