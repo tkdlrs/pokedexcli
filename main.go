27534f4a9c585ddff3a35b9e541d6fea13209949
tkdlrs <tkd.lsanchez@gmail.com>
2025-05-09 16:16:15 -0600

    After making the adjustments to match the solution files from 'Lesson 4: REPL' in the guided project.

--- main.go
+++ main.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scannerThing := bufio.NewScanner(os.Stdin)
-	//
-	fmt.Print("Pokedex > ")
-	for scannerThing.Scan() {
-		usersInput := scannerThing.Text()
-		//
-		fmt.Println("Your command was:", cleanInput(usersInput)[0])
-		//
-		fmt.Print("Pokedex > ")
-	}
-	if err := scannerThing.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,15 +1,31 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+		//
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+		//
+		commandName := words[0]
+		//
+		fmt.Printf("Your command was: %s\n", commandName)
+	}
+}
 
 func cleanInput(text string) []string {
-	var wordsSlice []string
-	//
-	removeWhiteSpace := strings.TrimSpace(text)
-	removeWhiteSpace = strings.ReplaceAll(removeWhiteSpace, "  ", " ")
-	lowercase := strings.ToLower(removeWhiteSpace)
-	splitWords := strings.Split(lowercase, " ")
-	wordsSlice = splitWords
-	//
-	return wordsSlice
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
 }
